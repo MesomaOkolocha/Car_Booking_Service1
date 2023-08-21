e75606741fa684b5f3683e278d7ee4db4a270c95
MesomaOkolocha <Tzmish10@gmail.com>
2023-08-21 14:37:29 -0400

    added DuplicateResourceException to handle instances where we already have an existing resource such as adding a newUser with the same name

---
+++ src/main/java/org/mesoma/utils/DuplicateResourceException.java
@@ -0,0 +1,12 @@
+package org.mesoma.utils;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT)
+public class DuplicateResourceException extends RuntimeException{
+    public DuplicateResourceException(String message){
+        super(message);
+    }
+}
