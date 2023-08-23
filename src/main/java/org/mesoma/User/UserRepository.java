f3f3238784c237a94de3508d3baa0a65357f6418
MesomaOkolocha <Tzmish10@gmail.com>
2023-08-23 15:19:36 -0400

    Added UserRepository Interface Extending JpaRepository to provide convenient methods for interacting with the database to perform CRUD operations on the User entity using Spring Data JPA.

---
+++ src/main/java/org/mesoma/User/UserRepository.java
@@ -0,0 +1,8 @@
+package org.mesoma.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
